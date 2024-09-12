3b777d1111e92bdb579528682534283d12f7e1a1
JeremiahSimo <jeremiahsimo26@gmail.com>
2024-09-12 11:02:56 +0800

    nag create ko ug signup

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="form-box">
+<form class="form">
+    <span class="title">Sign up</span>
+    <span class="subtitle">Create a free account with your email.</span>
+    <div class="form-container">
+      <input type="text" class="input" placeholder="Full Name">
+			<input type="email" class="input" placeholder="Email">
+			<input type="password" class="input" placeholder="Password">
+    </div>
+    <button>Sign up</button>
+</form>
+<div class="form-section">
+  <p>Have an account? <a href="">Log in</a> </p>
+</div>
+</div>
+</body>
+</html>
