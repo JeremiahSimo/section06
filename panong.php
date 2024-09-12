064b2ab37ebbd3325fcbdbf0ed7a2cf3c8dfbb07
unknown <saam.panong.coc@phinmaed.com>
2024-09-12 11:28:35 +0800

    humana ko

---
+++ panong.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="panong.css">
+    <title>Sign In</title>
+</head>
+<body>
+
+    <form class="form">
+    <p class="form-title">Sign in to your account</p>
+        <div class="input-container">
+        <input type="email" placeholder="Enter email">
+        <span>
+        </span>
+    </div>
+    <div class="input-container">
+        <input type="password" placeholder="Enter password">
+        </div>
+        <button type="submit" class="submit">
+        Sign in
+    </button>
+
+    <p class="signup-link">
+        No account?
+        <a href="">Sign up</a>
+    </p>
+    </form>
+
+</body>
+</html>
