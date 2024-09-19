44539e309c9f69b54b1becfdb8e83f60475631f9
unknown <saam.panong.coc@phinmaed.com>
2024-09-19 11:44:36 +0800

    gutom nako

---
+++ panong11.php
@@ -0,0 +1,33 @@
+<style>
+    /* From Uiverse.io by marcelodolza */ 
+.container {
+  width: 100%;
+  height: 100%;
+  --s: 100px; /* control the size */
+  --c1: #f8b195;
+  --c2: #355c7d;
+
+  --_g: var(--c2) 4% 14%, var(--c1) 14% 24%, var(--c2) 22% 34%,
+    var(--c1) 34% 44%, var(--c2) 44% 56%, var(--c1) 56% 66%, var(--c2) 66% 76%,
+    var(--c1) 76% 86%, var(--c2) 86% 96%;
+  background: radial-gradient(
+      100% 100% at 100% 0,
+      var(--c1) 4%,
+      var(--_g),
+      #0008 96%,
+      #0000
+    ),
+    radial-gradient(
+        100% 100% at 0 100%,
+        #0000,
+        #0008 4%,
+        var(--_g),
+        var(--c1) 96%
+      )
+      var(--c1);
+  background-size: var(--s) var(--s);
+}
+
+</style>
+
+<div class="container"></div>
